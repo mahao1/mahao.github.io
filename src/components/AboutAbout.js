7ed054a627f689bdf59c56c8c4bee3923c9956fa
mahao1 <546118915@qq.com>
2017-08-27 17:25:45 +0800

    Update AboutAbout.js

--- src/components/AboutAbout.js
+++ src/components/AboutAbout.js
@@ -18,30 +18,6 @@ class AboutAbout extends Component{
     }
     return (
       <div className="about">
-        {/*<div className="container">
-          <h3>LIFESTYLE</h3>
-          <div className="col-md-5 about-left">
-            <img className="img-responsive" src="/src/assets/images/8.jpg" alt=""/>
-          </div>
-          <div className="col-md-7 about-right">
-            <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum </p>
-            <p>The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum</p>
-          </div>
-          <div className="clearfix"></div>
-          <div className="abt-btm">
-            <div className="col-md-7 ab-lft">
-              <h3>OUR AGENCY ALL OVER THE WORLD</h3>
-              <h5>Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text.</h5>
-              <p>It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker It was popularised in the 1960s with the release of Letraset sheets including versions of Lorem Ipsum.</p>
-            </div>
-            <div className="col-md-5 ab-rgt">
-              <h3>PRESS</h3>
-              <p><span className="bec">1 . </span>Lorem Ipsum available, but the  have suffered alteration in some form, by injected humour, or randomised words which don't look. </p>
-              <p><span className="bec">2 . </span>Lorem Ipsum available, but the  have suffered alteration in some form, by injected humour, or randomised words which don't look. </p>
-            </div>
-            <div className="clearfix"></div>
-          </div>
-        </div>*/}
         {
           arr.map((item,index)=>{
             return (
