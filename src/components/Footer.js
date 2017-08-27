e7bf5369043297c93649b7f1a951ca94b04fdf57
mahao1 <546118915@qq.com>
2017-08-27 17:31:30 +0800

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,27 +6,11 @@ class Footer extends Component{
       <div className="footer">
         <div className="container">
           <div className="footer-main">
-            {/*<div className="col-md-4 ftr-gd">
-              <h3>Follow Us</h3>
-              <ul>
-                <li><a href="#" className="fa"> </a></li>
-                <li><a href="#" className="tw"> </a></li>
-                <li><a href="#" className="g"> </a></li>
-              </ul>
-            </div>*/}
             <div className="col-md-12 ftr-gd">
               <h3>Newsletter</h3>
               <input type="text" placeholder="Enter Your email" onFocus={this.value = ''} />
               <input type="submit" value="Subscribe"/>
             </div>
-            {/*<div className="col-md-4 ftr-gd">
-              <h3>Address</h3>
-              <address>
-                795 Folsom Ave, Suite 600<br/>
-                San Francisco, CA 94107<br/>
-                <abbr title="Phone">P :</abbr> (123) 456-7890
-              </address>
-            </div>*/}
             <div className="clearfix"> </div>
           </div>
           <div className="copyright">
